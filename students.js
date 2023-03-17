914c33e2f721e834045c9b585adaa212aa225f8a
Julio Muro <126353562+juliomuro@users.noreply.github.com>
2023-03-16 19:02:20 -0700

    added my info

--- students.js
+++ students.js
@@ -7,11 +7,11 @@ export const data = [
         imagePath:'images/davidLiang.jpg'
     },
     {
-        firstName:'David',
-        lastName:'Liang',
-        location: 'Philadephia, PA',
-        html:'davidLiang.html',
-        imagePath:'images/davidLiang.jpg'
+        firstName:'Julio',
+        lastName:'Muro',
+        location: 'Banning, CA',
+        html:'juliomuro.html',
+        imagePath:'images/julioMuro.jpg'
     },
 ]
 
