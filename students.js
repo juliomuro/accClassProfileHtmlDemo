d8a8a3a4a6879afcd2933b8d6a2dabe430d9c53d
Daniel Santoscoy <126369010+DanielSantoscoy@users.noreply.github.com>
2023-03-16 19:06:37 -0700

    Update students.js
    Added my profile.

--- students.js
+++ students.js
@@ -7,11 +7,11 @@ export const data = [
         imagePath:'images/davidLiang.jpg'
     },
     {
-        firstName:'David',
-        lastName:'Liang',
-        location: 'Philadephia, PA',
-        html:'davidLiang.html',
-        imagePath:'images/davidLiang.jpg'
+        firstName:'Daniel',
+        lastName:'Santoscoy',
+        location: 'San Jacinto, CA',
+        html:'danielSantoscoy.html',
+        imagePath:'images/danielSantoscoy.jpg'
     },
 ]
 
